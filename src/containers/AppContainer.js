36fcdf84d1312795d36f1b205cf9aa36d33eb8d3
Vu Le <vule@teqnological.asia>
2022-01-26 15:55:17 +0700

    update AppContainer.js

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -24,29 +24,23 @@ class AppContainer extends Component {
     const currentPath = window.location.pathname;
 
     if (sessionStorage.getItem('token') !== null && currentPath.match('/account')) {
-      const url = `${process.env.REACT_APP_OPENACCOUNT_API_HOST}/v3/accounts/status`;
+      const url = `${process.env.REACT_APP_ACCOUNT_MANAGER_API}/v4/accountStatus`
       const options = {
         headers: getAuthHeader()
-      };
-      axios.get(url, options).then(({ data: { data: { items } } }) => {
-        const {
-          account_status, identification_status, progress_status,
-          posted_status, identification_messages, profile_messages
-        } = items;
-        if (account_status === 'available') {
-          sessionStorage.setItem('account_status', account_status)
-        } else {
-          sessionStorage.setItem('account_status', account_status);
-          sessionStorage.setItem('identification_status', identification_status);
-          sessionStorage.setItem('progress_status', progress_status);
-          sessionStorage.setItem('posted_status', posted_status);
-          sessionStorage.setItem('identification_messages', JSON.stringify(identification_messages));
-          sessionStorage.setItem('profile_messages', JSON.stringify(profile_messages));
-          sessionStorage.setItem('path', '/openaccount/check-status');
-          window.location.href = '/op/index.html'
+      }
+      return axios.get(url, options)
+        .then(({ data: items }) => {
+          const { equity } = items.status
+          if (equity === 'AVAILABLE') {
+            const res = equity.toLowerCase()
+            sessionStorage.setItem('account_status', res)
+          } else {
+            const redirectUri = `${process.env.PUBLIC_URL || 'http://localhost:3000'}/account-state?from=bcplogin`
+            window.location.href=redirectUri
+          }
+        })
+        .catch(error => {})
         }
-      }).catch(error => {})
-    }
   }
 
   render() {
