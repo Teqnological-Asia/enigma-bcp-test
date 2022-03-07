fbc13cdc1aca077a7be6dc453d5aa3cc6494b476
teq-hungthanh <hungthanh@teqnological.asia>
2022-03-07 09:16:57 +0700

    change text

--- src/components/Home/ProfileDetail.js
+++ src/components/Home/ProfileDetail.js
@@ -4,12 +4,13 @@ const ProfileDetail = ({profile}) => {
   if (profile) {
     return (
       <div className="p-section_user u-mt40p">
-        <div className="p-section_user_name">{`${profile.name_kanji} 様`}</div>
+        {/*<div className="p-section_user_name">{`${profile.name_kanji} 様`}</div>*/}
+        <div className="p-section_user_name">様</div>
         <dl className="p-section_user_code">
           <dt>あなたの加入者口座コード</dt>
-          <dd>{profile.jasdec_code}</dd>
+          {/*<dd>{profile.jasdec_code}</dd>*/}
           <dt>部店-口座番号</dt>
-          <dd>{profile.branch_code}-{profile.account_id}</dd>
+          {/*<dd>{profile.branch_code}-{profile.account_id}</dd>*/}
         </dl>
       </div>
     );
