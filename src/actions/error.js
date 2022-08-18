fdab7e757890dc94c1659170e86fb5ac45993930
Le Van Vu <vule@teqnological.asia>
2022-08-18 14:14:28 +0700

    handle api err with 503 code

--- src/actions/error.js
+++ src/actions/error.js
@@ -2,11 +2,14 @@ import { CREATE_ERROR, DELETE_ERROR } from '../constants/error';
 import { invalidToken } from './auth';
 import { isTokenExpired } from '../utils';
 import { setLoading } from '../actions/loading';
+import { push } from 'react-router-redux';
+import { logoutSuccess } from './auth';
 
 export const handleErrors = (error, lastAction) => (dispatch, getState) => {
   if (error.response) {
     let errorMessage = '';
     const errorCode = error.response.data.code;
+    const errorStatus = error.response ? error.response.status : null;
     const errorMsg = error.response.data.message;
 
     dispatch(setLoading(false))
@@ -14,7 +17,15 @@ export const handleErrors = (error, lastAction) => (dispatch, getState) => {
       dispatch(invalidToken());
     } else {
       errorMessage = errorCode ? `${errorMsg} [${error.response.data.code}]` : errorMsg;
-      dispatch(createError(errorMessage));
+      if(errorStatus === 503) {
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('currentAccountType');
+        dispatch(logoutSuccess());
+        dispatch(push('/account/go-to-login'));
+      }
+      else{
+      dispatch(createError(errorMessage))
+      }
     }
   }
 }
